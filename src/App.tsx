9e4debcbd62aa281109c65cc042e3ebd12843c05
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2026-02-21 15:08:31 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-mint-500 p-4 m-10 border-2 hover:bg-mint-500 hover:text-white">
+      Hello nova group
+    </div>
+  );
+}
+
+export default App;
