bc9c9bd3496dd3b0b96442af6dc8523e42dde372
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2026-02-21 16:39:24 +0200

    add api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 import { useAuthStore } from "../features/auth/useAuthStore";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://api.example.com",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://production.thenovagroupco.com/api/v1",
   headers: { "Content-Type": "application/json" },
 });
 
