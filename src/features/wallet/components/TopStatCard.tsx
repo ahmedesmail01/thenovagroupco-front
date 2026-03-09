5ca0fc4910a72bc088765bbf6eda02ea31ed95fb
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2026-03-09 11:45:19 +0200

    feat: implement wallet feature with dedicated components for earnings, transactions, withdrawals, balances, and charts, and integrate into navigation.

--- src/features/wallet/components/TopStatCard.tsx
+++ src/features/wallet/components/TopStatCard.tsx
@@ -8,14 +8,16 @@ export function TopStatCard({
   accentClass: string;
 }) {
   return (
-    <div className="bg-white rounded-xl p-6 py-8 shadow-sm border border-slate-50 flex flex-col justify-center relative overflow-hidden h-32">
-      {/* Accent Line */}
+    <div className="bg-white rounded-[16px] px-[32px] py-[10px] shadow-[0_8px_30px_rgb(0,0,0,0.04)] border border-slate-50 flex flex-col justify-center relative overflow-hidden h-34">
+      {/* Accent Bar */}
       <div
-        className={`absolute left-0 top-1/2 -translate-y-1/2 h-10 w-1 rounded-r-md ${accentClass}`}
+        className={`absolute left-0 top-1/3 -translate-y-1/2 h-14 w-[6px] rounded-r-full ${accentClass}`}
       />
-      <div className="pl-3 space-y-1.5">
-        <h3 className="text-slate-500 text-sm font-medium">{title}</h3>
-        <p className="text-slate-800 text-[26px] font-semibold tracking-wide">
+      <div className=" space-y-2">
+        <h3 className="text-[#8e8ea1] text-lg font-medium tracking-tight">
+          {title}
+        </h3>
+        <p className="text-[#1e1b39] text-[24px] font-bold tracking-tight">
           {value}
         </p>
       </div>
