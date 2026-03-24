09093c86dd3de65388ce9fd99e463ce165909b1f
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2026-03-24 02:19:12 +0200

    feat: Add `BottomStatsRow` component to display key user statistics on the dashboard.

--- src/components/dashboard/BottomStatsRow.tsx
+++ src/components/dashboard/BottomStatsRow.tsx
@@ -51,7 +51,7 @@ export default function BottomStatsRow({ data }: BottomStatsRowProps) {
     },
     {
       iconSrc: "/icons/silver-pack-icon.png",
-      title: data?.rank?.package || "Executive",
+      title: data?.rank?.package || "No Rank",
       label: "Next Rank",
     },
     {
