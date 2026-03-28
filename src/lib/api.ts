83e015b9f189fd81a8161b3745e2b278581db899
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2026-03-28 17:31:00 +0200

    restore

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,19 +6,20 @@ const api = axios.create({
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
+    "X-XSRF-TOKEN": Cookies.get("XSRF-TOKEN"),
   },
   withCredentials: true,
   withXSRFToken: true,
 });
 
-api.interceptors.request.use((config) => {
-  const token = Cookies.get("XSRF-TOKEN");
-  console.log("🔑 XSRF-TOKEN:", token);
-  console.log("🍪 All cookies:", document.cookie);
-  if (token) {
-    config.headers["X-XSRF-TOKEN"] = token;
-  }
-  return config;
-});
+// api.interceptors.request.use((config) => {
+//   const token = Cookies.get("XSRF-TOKEN");
+//   console.log("🔑 XSRF-TOKEN:", token);
+//   console.log("🍪 All cookies:", document.cookie);
+//   if (token) {
+//     config.headers["X-XSRF-TOKEN"] = token;
+//   }
+//   return config;
+// });
 
 export default api;
