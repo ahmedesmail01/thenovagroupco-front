ba22a402769d84c09f1325646e89a40200364c6d
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2026-03-28 22:52:38 +0200

    feat: add getCookie utility for CSRF token retrieval and update environment variable configuration

--- src/lib/getCookie.ts
+++ src/lib/getCookie.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
 
 export const getCookie = () =>
-  axios.get(`${import.meta.env.VITE_API_BASE}/sanctum/csrf-cookie`, {
+  axios.get(`/sanctum/csrf-cookie`, {
     withCredentials: true,
   });
+
+//for local dev
+
+// export const getCookie = () =>
+//   axios.get(`${import.meta.env.VITE_API_BASE}/sanctum/csrf-cookie`, {
+//     withCredentials: true,
+//   });
