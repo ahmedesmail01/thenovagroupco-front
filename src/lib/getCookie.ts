b982414bad127c53e49d53bccb81fb63cbd2b8ff
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2026-04-03 20:03:03 +0200

    feat: implement CSRF cookie utility and update API environment configuration

--- src/lib/getCookie.ts
+++ src/lib/getCookie.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-// export const getCookie = () =>
-//   axios.get(`/sanctum/csrf-cookie`, {
-//     withCredentials: true,
-//   });
+export const getCookie = () =>
+  axios.get(`/sanctum/csrf-cookie`, {
+    withCredentials: true,
+  });
 
 //for local dev
 
-export const getCookie = () =>
-  axios.get(`${import.meta.env.VITE_API_BASE}/sanctum/csrf-cookie`, {
-    withCredentials: true,
-  });
+// export const getCookie = () =>
+//   axios.get(`${import.meta.env.VITE_API_BASE}/sanctum/csrf-cookie`, {
+//     withCredentials: true,
+//   });
